9c90ae9b59e19a6c1064d4a495ed178e4f2b1e45
Nastyfrisky <stpnkvnst@mail.ru>
2022-02-11 19:49:35 +0300

    Initial Commit

---
+++ CalculatorBMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorBMI
+//
+//  Created by Анастасия Ступникова on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
