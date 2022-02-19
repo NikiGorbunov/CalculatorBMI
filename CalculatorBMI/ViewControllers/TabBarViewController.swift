b5ecf1b3abb1942703c67ddea7df886797433bfc
Nastyfrisky <stpnkvnst@mail.ru>
2022-02-19 19:03:10 +0300

    Tab bar fix

---
+++ CalculatorBMI/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  CalculatorBMI
+//
+//  Created by Анастасия Ступникова on 19.02.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
+    }
+    
+
+}
